1fe7c140af2cf6e064fa796f36f10c371c41b092
jeanhar <jhmartinez55@misena.edu.co>
2024-04-02 11:24:59 -0500

    proyecto login

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="validar.php" method="post">
+    <h1> Sistema de login </h1>
+    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
+   
+   <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+    <input type="submit" value="Ingresar">
+    <input type="submit" value="Ingresar">
+       
+    <form action="validar.php" method="post">
+
+    <form action="validar.php" method="post">
+
+<h1 >Sistema de login</h1>
+
+<p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
+
+<p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+<input type="submit" value="Ingresar"> 
+ 
+<form action="validar.php" method="post">  
+
+<h1 >Sistema de login</h1>
+
+<p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
+
+<p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+<input type="submit" value="Ingresar">
+
+</form>
+</body>
+</html>
